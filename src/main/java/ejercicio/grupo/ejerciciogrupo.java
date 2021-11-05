678444ca23753d7f7d8633a404fc19e0425efac1
franjmg84 <f.jesusmg84@gmail.com>
2021-11-05 17:05:13 +0100

    nuevo
    nuevo

--- src/main/java/ejercicio/grupo/ejerciciogrupo.java
+++ src/main/java/ejercicio/grupo/ejerciciogrupo.java
@@ -6,14 +6,14 @@ public class ejerciciogrupo {
 	System.out.println("hola grupo");
 	System.out.println("Empezamos la historia");
 	System.out.println("PETTER estaba en su casa y estaba viendo la tv y algo paso");
-	System.out.println("La tele se lo comio y lo metio en un videojuego")
+	System.out.println("La tele se lo comio y lo metio en un videojuego");
 	
 	System.out.println("El personaje antagonico se llama Miguel el programador");
 	System.out.println("Se metio en el mejor videojuego a mundo abierto");
-	System.out.println("PETTER solo tenia control de la tele como defensa contra miguel y sus secuaces")
-	System.out.println("Y para ganarle necesitaba aplicar numerosos conocimientos de programacion")
+	System.out.println("PETTER solo tenia control de la tele como defensa contra miguel y sus secuaces");
+	System.out.println("Y para ganarle necesitaba aplicar numerosos conocimientos de programacion");
 	System.out.println("PETTER lucharía con su ultrapoder dentro de la TV, manejaría con su dedo indice a sus enemigos pero solo de cintura hacia abajo.");
-	
+	System.out.println("En el interior de la TV no tiene a nadie que pueda con el");
 	
 	}
 }
