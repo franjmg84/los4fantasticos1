f12c4a2a3d218c89b2e248a5e952ad8aefe2bf3b
franjmg84 <f.jesusmg84@gmail.com>
2021-11-05 17:25:35 +0100

    otro
    otro

--- src/main/java/ejercicio/grupo/ejerciciogrupo.java
+++ src/main/java/ejercicio/grupo/ejerciciogrupo.java
@@ -17,8 +17,9 @@ public class ejerciciogrupo {
 	System.out.println("El mando de PETTER solo tenia 1 dia de duracion asi que PETTER intento buscar amigos , para luchar contra el gran miguel");
 	System.out.println("Asi que PETTER recluto a varios nomos que habia en un campo desolado");
 	System.out.println("El nomo Lorenzo, Jose el abuelo nomo y fran el nomo DJ avisaron al gran nomo Emilio para que ayudaran a PETTER en su misión");
-	System.out.println("Y cuando ivan a empezar la lucha se apago la TV");
+	System.out.println("Y cuando iban a empezar la lucha se apago la TV");
 	System.out.println("Había sido Miguel el programador que vio que su reinado en Andorrland podría acabar con PETTER y sus nomos");
-	System.out.println("A si que PETTER volvio a la realidad pero esto no acabaria asi")
+	System.out.println("A si que PETTER volvio a la realidad pero esto no acabaria asi");
+	System.out.println("El nomo dj monto su equipo y se puso a pinchar y el nomo emilio se desato y se quedo en bolas");
 	}
 }
