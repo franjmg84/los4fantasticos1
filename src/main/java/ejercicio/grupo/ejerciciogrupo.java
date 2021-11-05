4ed7542ed4c90018884fc3043e2025f9c55ba66e
lorenzoea12 <92109963+lorenzoea12@users.noreply.github.com>
2021-11-05 16:51:08 +0100

    primera intervencion lorenzo

--- src/main/java/ejercicio/grupo/ejerciciogrupo.java
+++ src/main/java/ejercicio/grupo/ejerciciogrupo.java
@@ -6,7 +6,6 @@ public class ejerciciogrupo {
 	System.out.println("hola grupo");
 	System.out.println("Empezamos la historia");
 	System.out.println("PETTER estaba en su casa y estaba viendo la tv y algo paso");
-	
-
-}
+	System.out.println("La tele se lo comio y lo metio en un videojuego")
+	}
 }
