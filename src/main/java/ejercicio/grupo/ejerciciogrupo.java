3723e138803b24b2778f75d541c5b98724f81e9e
Emilio <92530932+cambiocds@users.noreply.github.com>
2021-11-05 16:52:41 +0100

    siguiente linea

--- src/main/java/ejercicio/grupo/ejerciciogrupo.java
+++ src/main/java/ejercicio/grupo/ejerciciogrupo.java
@@ -7,5 +7,7 @@ public class ejerciciogrupo {
 	System.out.println("Empezamos la historia");
 	System.out.println("PETTER estaba en su casa y estaba viendo la tv y algo paso");
 	System.out.println("La tele se lo comio y lo metio en un videojuego")
+	
+	System.out.println("El personaje antagonico se llama Miguel el programador");
 	}
 }
